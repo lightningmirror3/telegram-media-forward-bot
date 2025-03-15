806ab1c8044526e129b0b0a97f23b7ba5be8b0b1
lightningmirror3 <lightningmirror3@gmail.com>
2025-03-15 09:28:35 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+
+CHANNEL_MAPPING = {
+    -1002386644256: -1002484982348,
+    -1001597273610: -1001721796359,
+    -1002261820786: -1002255696539,
+    -1002418710282: -1002292610792,
+    -1002100804603: -1002334248978,
+}
+
+ALLOWED_MIME_TYPES = {"video/x-matroska", "video/mp4"}
